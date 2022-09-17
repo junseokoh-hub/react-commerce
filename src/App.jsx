54da1eca84588e8ea1c969a90acd4b1da7af5ae8
junseokoh-hub <lunulatecat@naver.com>
2022-09-17 18:46:02 +0900

    initial settings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GlobalStyle from "./GlobalStyle";
+import Router from "./Router";
+
+function App() {
+  return (
+    <>
+      <Router />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
