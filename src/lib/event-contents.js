b27025b09314955e40fdd9c2cf216dcb1a3af0bc
junseokoh-hub <lunulatecat@naver.com>
2022-10-06 20:51:46 +0900

    decorate event detail component

--- src/lib/event-contents.js
+++ src/lib/event-contents.js
@@ -17,9 +17,13 @@ export const EVENT_CONTENTS = [
       third: oldBookImage3,
     },
     description: {
-      title: "hello",
-      content: "this is hello",
-      end: "goodbye",
+      title: "헌책 나눔 이벤트",
+      content:
+        "저희가 가지고 있는 오래된 책들에 한 해서 무료로 나눠드리는 이벤트를 개최하게 되었습니다. 필요한 책이 있으시면 이벤트에 꼭 참여해주시길 바랍니다. ",
+      end: {
+        date: "2023년 1월 1일 ~ 2023년 1월 31일",
+        subjected: "사이트 가입자 아무나",
+      },
     },
   },
   {
@@ -30,9 +34,13 @@ export const EVENT_CONTENTS = [
       third: newProductImage3,
     },
     description: {
-      title: "hello",
-      content: "this is hello",
-      end: "goodbye",
+      title: "신상품 우선 신청 이벤트",
+      content:
+        "12월 저희의 신상품과 함께 새로운 이벤트를 선사드립니다! 새로운 기기 프로모션과 함께 여러분께서 기존 가격보다 더 낮은 가격으로 선 구매를 하실 수 있는 기회를 잡으실 수 있습니다. 만약 원하는 기기를 선구매 하고 싶으신 분들께서는 꼭 이 이벤트에 신청해서 구매하시길 바랍니다. 여러분께 드리는 저희의 마음이 꼭 전달되기를 바랍니다.",
+      end: {
+        date: "2022년 12월 24일 ~ 2023년 1월 1일",
+        subjected: "사이트 가입자 아무나",
+      },
     },
   },
   {
@@ -43,9 +51,13 @@ export const EVENT_CONTENTS = [
       third: bookConcertImage3,
     },
     description: {
-      title: "hello",
-      content: "this is hello",
-      end: "goodbye",
+      title: "북 콘서트 개최 행사 이벤트",
+      content:
+        "다양한 저자들과 다양한 독자들과 함께 책이란 무엇인가, 책에 대하여 논의하고 싶으신 분들께 저희가 권해드립니다! 오프라인 개최로 책을 사랑하시는 분들이 모여 이야기 꽃을 펼치는 장에 모실 수 있는 기회가 바로 여기있습니다! 책에 대한 소통이 이루어지는 이번 이벤트에 많이 참가해주세요!",
+      end: {
+        date: "2023년 2월 1일 ~ 2023년 2월 15일",
+        subjected: "사이트 가입자 아무나",
+      },
     },
   },
 ];
--- src/pages/Community/EventDetailPage.jsx
+++ src/pages/Community/EventDetailPage.jsx
@@ -1,36 +1,117 @@
 import React from "react";
 import styled from "styled-components";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { EVENT_CONTENTS } from "../../lib/event-contents";
+import { BsBoxArrowLeft } from "react-icons/bs";
 
 const EventDetailWrapper = styled.div`
+  position: relative;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  .backwards {
+    position: absolute;
+    top: 20px;
+    left: 40px;
+    font-size: 30px;
+    cursor: pointer;
+  }
 `;
 
 const EventDetailContainer = styled.div`
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   img {
     display: block;
     width: 50vw;
-    height: 70vh;
+    height: 90vh;
+  }
+  @media screen and (max-width: 768px) {
+    img {
+      width: 55vw;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    img {
+      width: 80vw;
+    }
+  }
+`;
+
+const EventDetailTitle = styled.h3`
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  border-bottom: 4px solid ${(props) => props.theme.yellow.lighter};
+  font-size: 30px;
+  font-weight: bolder;
+  text-align: center;
+`;
+
+const EventDetailDesc = styled.div`
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${(props) => props.theme.brown.normal};
+  color: #ddd;
+  span {
+    margin: 15px 0;
+  }
+`;
+
+const EventDetailContent = styled.p`
+  margin: 20px 0;
+  width: 50%;
+  font-size: 18px;
+  font-weight: bolder;
+  line-height: 1.5;
+`;
+
+const EventBtnContainer = styled.div`
+  margin-bottom: 10px;
+  width: 50vw;
+  display: flex;
+  justify-content: flex-end;
+  button {
+    width: 100px;
+    height: 40px;
+    font-weight: bold;
+    background-color: ${(props) => props.theme.orange.normal};
+    border: 1px solid ${(props) => props.theme.orange.lighter};
+    color: ${(props) => props.theme.whiteColor};
+    &:active {
+      background-color: ${(props) => props.theme.brown.normal};
+    }
   }
 `;
 
 const EventDetailPage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const EventDetail = EVENT_CONTENTS.find((item) => item.id === id);
 
-  const { id: detailId, detailImage, description } = EventDetail;
+  const { id: detailId, image: detailImage, description } = EventDetail;
 
   return (
     <EventDetailWrapper>
+      <BsBoxArrowLeft className="backwards" onClick={() => navigate(-1)} />
       <EventDetailContainer>
-        <img src={detailImage.first} alt={detailId} />
-        <img src={detailImage.second} alt={detailId} />
-        <img src={detailImage.third} alt={detailId} />
-        <span>{detailId}</span>
-        <p>{description.title}</p>
+        <EventDetailTitle>{description?.title}</EventDetailTitle>
+        <img src={detailImage?.first} alt={detailId} />
+        <img src={detailImage?.second} alt={detailId} />
+        <img src={detailImage?.third} alt={detailId} />
+        <EventDetailDesc>
+          <EventDetailContent>{description?.content}</EventDetailContent>
+          <span>일자 : {description?.end?.date}</span>
+          <span>대상 : {description?.end?.subjected}</span>
+        </EventDetailDesc>
       </EventDetailContainer>
+      <EventBtnContainer>
+        <button>신청하기</button>
+      </EventBtnContainer>
     </EventDetailWrapper>
   );
 };
